9685b60ec1386d242834eb6ab41116d2fcbe7639
AndroidHQ254 <d.murimi14@gmail.com>
2021-07-18 01:55:29 +0300

    sm6250-common: Explicitly define not to ignore NEVERALLOWS
    system/sepolicy/Android.mk:91: error: SELINUX_IGNORE_NEVERALLOWS := true cannot be used in user builds.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -205,7 +205,7 @@ include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
-SELINUX_IGNORE_NEVERALLOWS := true
+SELINUX_IGNORE_NEVERALLOWS := false
 
 # Shims
 TARGET_LD_SHIM_LIBS := vendor/lib/hw/audio.primary.atoll.so|fakelogprint.so
